7b8f1cf048c376d6752392ed6f2e7ba390591d60
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 12:37:19 +0800

    print the result of the code

--- exercise 7.py
+++ exercise 7.py
@@ -7,3 +7,6 @@ def word_counter(sentence, word):
 str_x = "Emma is a good developer. Emma is a writer"
 # make a variable for the word that you want to count in the sentence 
 the_word = "Emma"
+
+# print the result 
+print(f"{the_word} appeared {word_counter(str_x,the_word)} times")
